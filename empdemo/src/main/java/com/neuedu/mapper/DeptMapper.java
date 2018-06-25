0af1c6e011b817ab5182ce37657dc49f0802c22b
GetingUp <37234045+GetingUp@users.noreply.github.com>
2018-06-25 20:34:05 +0800

    Initial commit

---
+++ empdemo/src/main/java/com/neuedu/mapper/DeptMapper.java
@@ -0,0 +1,15 @@
+package com.neuedu.mapper;
+
+import com.neuedu.entity.Dept;
+import com.neuedu.entity.Emp;
+
+import java.util.List;
+
+public interface DeptMapper {
+
+    /**
+     * 查询所有dept
+     * @return
+     */
+    List<Dept> listDept();
+}
